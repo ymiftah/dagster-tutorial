ac26fc621c41b7f2ce7e78449788edc57f708df8
Youssef Miftah <miftahyo@outlook.fr>
2025-11-03 12:12:04 +1100

    test ci

--- tests/test_ci.py
+++ tests/test_ci.py
@@ -8,15 +8,15 @@ import requests
 
 from dagster import file_relative_path
 
-IS_BUILDKITE = os.getenv("BUILDKITE") is not None
+IS_CI = os.getenv("IS_CI") is not None
 
-HOST_NAME = "localhost"
-PORT = "3000"
+HOST_NAME = os.getenv("WEBSERVER_NAME", "localhost")
+PORT = os.getenv("WEBSERVER_PORT", "3000")
 
 @contextmanager
 def docker_service_up(docker_compose_file):
-    if IS_BUILDKITE:
-        yield  # buildkite pipeline handles the service
+    if IS_CI:
+        yield  # GHA pipeline handles the service
         return
 
     try:
@@ -43,10 +43,23 @@ def docker_service_up(docker_compose_file):
 def compose_up():
     with docker_service_up(file_relative_path(__file__, "../docker-compose.yml")):
         # Wait for server to wake up
-        # start_time = time.time()
-        time.sleep(20)
+        start_time = time.time()
+        while True:
+            try:
+                response = requests.get(f"http://{HOST_NAME}:{PORT}/")
+                if response.status_code == 200:
+                    break
+            except:
+                time.sleep(3)
+            if time.time() - start_time > 30:
+                raise TimeoutError("Docker service is not up.")
         yield
 
 def test_status(compose_up):
     response = requests.get(f"http://{HOST_NAME}:{PORT}/")
+    assert response.status_code == 200
+
+def test_run(compose_up):
+    response = requests.get(f"http://{HOST_NAME}:{PORT}/")
+    # Replace this, run a job and report status
     assert response.status_code == 200
